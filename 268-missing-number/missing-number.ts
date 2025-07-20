84051fb794fde5d0813b8d7d4073e426e7200181
sbharti-sudo <swatibharti1510@gmail.com>
2025-07-20 18:47:57 +1000

    Time: 0 ms (100.00%) | Memory: 57.1 MB (89.77%) - LeetSync

---
+++ 268-missing-number/missing-number.ts
@@ -0,0 +1,12 @@
+function missingNumber(nums: number[]): number {
+    let n = nums.length 
+    let expectedSum = (n *(n+1))/2;
+
+    let actualSum = 0;
+
+    for(let i = 0; i < n; i++){
+        actualSum = actualSum + nums[i]
+    }
+
+    return expectedSum - actualSum
+};
