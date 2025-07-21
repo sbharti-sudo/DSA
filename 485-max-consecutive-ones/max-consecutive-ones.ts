3bd06c03f21376bb2d54f06c781873635b47c7d6
sbharti-sudo <swatibharti1510@gmail.com>
2025-07-21 21:17:00 +1000

    Time: 1 ms (91.41%) | Memory: 61.4 MB (23.37%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.ts
@@ -0,0 +1,14 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let max = 0;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            count++
+            max = Math.max(max, count)
+        } else {
+            count = 0;
+        }
+    }
+    return max
+};
