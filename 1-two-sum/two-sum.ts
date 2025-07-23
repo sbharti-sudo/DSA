e05c4bc87068849cd1fd32077a713c6b752ccb44
sbharti-sudo <swatibharti1510@gmail.com>
2025-07-23 16:21:31 +1000

    Time: 0 ms (100.00%) | Memory: 56.1 MB (79.64%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,11 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
-    const mapCount:Record<number,number> = {};
-    for(let i = 0; i<nums.length; i++){
-        const diff = target - nums[i]
-        if(mapCount[diff]!== undefined){
-            return [mapCount[diff], i]
+    const mapRecord: Record<number, number> = {} // stores number and there indices
+
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i] //  diff required  to be added to nums[i] to make  target
+        if (mapRecord[diff] !== undefined) {
+            return [mapRecord[diff], i]
         }
-        mapCount[nums[i]] = i;
+        mapRecord[nums[i]] = i;
     }
     return []
 };
