e1aead49b54aba9a768714e2e02ab491e20c13be
sbharti-sudo <swatibharti1510@gmail.com>
2026-01-08 15:55:11 +1100

    Time: 2 ms (49.99%) | Memory: 56.8 MB (33.33%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,20 +1,20 @@
 function longestCommonPrefix(strs: string[]): string {
 
-    if(strs.length === 0){
+    if (strs.length === 0) {
         return ""
     }
 
     let first = strs[0]
-    for(let i = 0; i < first.length ; i++){
+    for (let i = 0; i < first.length; i++) {
 
         let ch = first[i]
 
-    for(let j = 1; j < strs.length; j++){
-        if(i>=strs[j].length || strs[j][i] !== ch ){
-            return first.slice(0,i)
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== ch) {
+                return first.slice(0, i)
 
+            }
         }
-    }
     }
     return first
 };
