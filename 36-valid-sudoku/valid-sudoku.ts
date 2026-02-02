ec98738d980429435ce709050bd4e090477cffd0
sbharti-sudo <swatibharti1510@gmail.com>
2026-02-02 22:12:07 +1100

    Time: 3 ms (88.57%) | Memory: 58.8 MB (70.15%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.ts
+++ 36-valid-sudoku/valid-sudoku.ts
@@ -6,11 +6,11 @@ function isValidSudoku(board: string[][]): boolean {
         for (let j = 0; j < 9; j++) {
             const val = board[i][j]
             if (val === ".") continue
-
+            // for each value we create this three strings below
             let rowKey = `${val}_ROW_${i}`;
             let colKey = `${val}_COL_${j}`;
-            let boxKey = `${val}_BOX_${Math.floor(i / 3)}_${Math.floor(j / 3)}`;
-
+            let boxKey = `${val}_BOX_${Math.floor(i / 3)}_${Math.floor(j / 3)}`; // any value in the 3 by 3 box divided by 3 will give same val.
+            // for each value first check if the strings that we created for this value already exist in see set. if not store those three strings for each value.
             if (seen.has(rowKey) || seen.has(colKey) || seen.has(boxKey)) {
                 return false
             }
