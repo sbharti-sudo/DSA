e8e0451068c7ab830533ab3eb378ff9f6564d9ce
sbharti-sudo <swatibharti1510@gmail.com>
2026-02-02 15:48:21 +1100

    Time: 13 ms (53.79%) | Memory: 62.2 MB (40.83%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -1,24 +1,28 @@
 function topKFrequent(nums: number[], k: number): number[] {
+    const freqMap = new Map<number, number>();
 
-    let freqMap: Record<number, number> = {};
-
-    for (let i = 0; i <= nums.length; i++) {
-        freqMap[nums[i]] = (freqMap[nums[i]] || 0) + 1
+    // 1) Count frequencies
+    for (let i = 0; i < nums.length; i++) {
+        const x = nums[i];
+        freqMap.set(x, (freqMap.get(x) ?? 0) + 1);
     }
 
-    let bucket: number[][] = Array(nums.length + 1).fill(null).map(() => []);
+    // 2) Bucket array where index = frequency
+    const bucket: number[][] = Array.from({ length: nums.length + 1 }, () => []);
 
-    for (let numStr in freqMap) {
-        let freq = freqMap[numStr];
-        bucket[freq].push(Number(numStr))
+    // 3) Put numbers into buckets by frequency
+    for (const [num, freq] of freqMap) {
+        bucket[freq].push(num);
     }
+
+    // 4) Collect from highest frequency to lowest until we have k
     const result: number[] = [];
-      for (let i = bucket.length - 1; i >= 0 && result.length < k; i--) {
-        for (const num of bucket[i]) {
+    for (let f = bucket.length - 1; f >= 0 && result.length < k; f--) {
+        for (const num of bucket[f]) {
             result.push(num);
             if (result.length === k) break;
         }
     }
-    return result
 
-};
+    return result;
+}
