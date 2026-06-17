0bba33bd7978d54e175015a2092f58d9ca0ea822
sbharti-sudo <swatibharti1510@gmail.com>
2026-06-17 15:06:27 +1000

    Time: 1 ms (87.17%) | Memory: 57.8 MB (23.40%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,15 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
 
-    const seen = new Map<number, number>();// number -> index
+    let seen = new Map<number, number>();// number: index
 
     for (let i = 0; i < nums.length; i++) {
-        let compliment = target - nums[i];
-
-        if (seen.has(compliment)) {
-            return [seen.get(compliment)!, i]
+        let compNumber = target - nums[i]
+        if (seen.has(compNumber)) {
+            return [seen.get(compNumber)!, i]
         }
-
-        seen.set(nums[i], i);
+        seen.set(nums[i], i)
     }
     return []
 }
