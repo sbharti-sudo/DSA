cb6fad8123b1e9d785eedbe6c8ec5032385bf92f
sbharti-sudo <swatibharti1510@gmail.com>
2026-06-22 17:00:20 +1000

    Time: 3 ms (53.49%) | Memory: 66.3 MB (25.48%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,13 @@
+function maxProfit(prices: number[]): number {
+    let minPrice = Infinity
+    let maxProfit = 0
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i]
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+    }
+    return maxProfit
+
+};
