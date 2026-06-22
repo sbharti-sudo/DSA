f1ec26a8e2bb79be9f7f0c59bd25001d5df77a92
sbharti-sudo <swatibharti1510@gmail.com>
2026-06-22 14:18:44 +1000

    Time: 32 ms (48.58%) | Memory: 67.7 MB (74.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.ts
+++ 49-group-anagrams/group-anagrams.ts
@@ -1,16 +1,25 @@
 function groupAnagrams(strs: string[]): string[][] {
-    const map = new Map<string, string[]>()
-    for (let s of strs) {
-        let count = new Array(26).fill(0)
-        for (let i = 0; i < s.length; i++) {
-            count[s.charCodeAt(i) - 97]++
-        }
-        let key = count.join("#");
-        const bucket = map.get(key);
-        if (bucket) bucket.push(s)
-        else map.set(key, [s])
-    }
-    return [...map.values()]
+    let map = new Map<string, string[]>();
 
+    for (let word of strs) {
+        let counts = new Array(26).fill(0);
+        for (let ch of word) {
+            counts[ch.charCodeAt(0) - 97]++
+        }
+        let key = counts.join(",");
+
+        if (map.has(key)) {
+            map.get(key).push(word)
+        } else {
+            map.set(key, [word])
+        }
+    }
+
+    return [...map.values()];
 
 };
+
+// story: 
+// make a map from key -> list of words
+// for each word: build its count-key
+// if key seen, push; else start new list
