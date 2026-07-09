9fab4201a316e7079fbf558813804588a32fde0f
sbharti-sudo <swatibharti1510@gmail.com>
2026-07-09 14:02:30 +1000

    Time: 1 ms (75.42%) | Memory: 56.4 MB (51.39%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
@@ -1,11 +1,11 @@
 function maxProfit(prices: number[]): number {
-
-    let profit = 0;
+    let maxProfit = 0
 
     for (let i = 0; i < prices.length; i++) {
-        if (prices[i - 1] < prices[i]) {
-            profit += prices[i] - prices[i - 1];
+        if (prices[i + 1] > prices[i]) {
+            maxProfit = (prices[i + 1] - prices[i]) + maxProfit
         }
     }
-    return profit;
+    return maxProfit
+
 };
